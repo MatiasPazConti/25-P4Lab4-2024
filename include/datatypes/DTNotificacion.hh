554fdac902b0cf23c12dd7de9be3b2f8f17ac6f4
Matias Paz Conti <matiaspazconti2020@gmail.com>
2024-06-13 20:32:23 -0300

    DTNotificacion.hh definido

--- include/datatypes/DTNotificacion.hh
+++ include/datatypes/DTNotificacion.hh
@@ -1,4 +1,15 @@
 #ifndef DTNOTIFICACION
 #define DTNOTIFICACION
 
+class DTNotificacion
+{
+private:
+  std::string aviso;
+
+public:
+  std::string getAviso();
+  DTNotificacion(std::string);
+  ~DTNotificacion();
+};
+
 #endif
