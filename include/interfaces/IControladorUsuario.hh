83f3c7efcc4fe0eade2aa853f09bab079f86b381
Matias Paz Conti <matiaspazconti2020@gmail.com>
2024-06-13 20:09:26 -0300

    DTCliente.hh definido

--- include/interfaces/IControladorUsuario.hh
+++ include/interfaces/IControladorUsuario.hh
@@ -3,15 +3,20 @@
 
 #include "../datatypes/DTFecha.hh"
 #include "../datatypes/DTDireccion.hh"
+#include "../datatypes/DTCliente.hh"
 
 #include <iostream>
+#include <set>
 
 class IControladorUsuario
 {
 public:
   virtual void resgitrarDatosUsuario(std::string, std::string, DTFecha) = 0;
   virtual void resgitrarDatosCliente(std::string, DTDireccion) = 0;
-  virtual void resgitrarDatosVendedor() = 0;
+  virtual void resgitrarDatosVendedor(int) = 0;
+  virtual void altaNuevoUsuario() = 0;
+  virtual std::set<DTUsuario> listarUsuarios() = 0;
+  virtual std::set<DTCliente> listarClientes() = 0;
 };
 
 #endif
