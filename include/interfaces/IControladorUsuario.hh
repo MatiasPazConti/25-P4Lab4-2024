e1185916db3ac23c8a7d916f848a610d485a47e3
Matias Paz Conti <matiaspazconti2020@gmail.com>
2024-06-13 20:03:09 -0300

    DTDireccion.hh definida

--- include/interfaces/IControladorUsuario.hh
+++ include/interfaces/IControladorUsuario.hh
@@ -1,10 +1,17 @@
 #ifndef I_CONTROLADOR_USUARIO
 #define I_CONTROLADOR_USUARIO
 
+#include "../datatypes/DTFecha.hh"
+#include "../datatypes/DTDireccion.hh"
+
+#include <iostream>
+
 class IControladorUsuario
 {
 public:
-  // Insertar funcion() = 0
+  virtual void resgitrarDatosUsuario(std::string, std::string, DTFecha) = 0;
+  virtual void resgitrarDatosCliente(std::string, DTDireccion) = 0;
+  virtual void resgitrarDatosVendedor() = 0;
 };
 
 #endif
